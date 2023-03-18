a9f64a664d0a79f870b35653e739e82122a56d20
Audrey <audrey.pompeo@gmail.com>
2023-03-17 22:37:19 -0300

    correção da cor em dark ícone github

--- dark light form/assets/js/script.js
+++ dark light form/assets/js/script.js
@@ -1,18 +1,25 @@
 const mode = document.getElementById('mode_icon');
 
 mode.addEventListener('click', () => {
+    console.log("Clicou aqui")
+    
+
     const form = document.getElementById('login_form');
+    const github= document.getElementById('github');
 
     if(mode.classList.contains('fa-moon')) {
         mode.classList.remove('fa-moon');
         mode.classList.add('fa-sun');
-
+        github.style.filter='invert(1)';
         form.classList.add('dark');
         return ;
-    }
-    
+    } else {
+        github.style.filter='none'
+    } 
+
     mode.classList.remove('fa-sun');
     mode.classList.add('fa-moon');
 
     form.classList.remove('dark');
-});
+    
+})
